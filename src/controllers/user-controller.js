b155571a51ba756401dd6805fb3ffea228c4878e
Jcarri14 <j.carrion@students.salle.url.edu>
2021-07-20 12:02:16 +0200

    feat: update user routes and controller

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,28 +1,23 @@
 const db = require("../models");
 const { encryptString } = require("../utils/encrypt");
-const { generateResponse } = require("../utils/generateResponse");
 
 async function signUp(req, res, next) {
-  const { email, password, firstname, lastname } = req.body;
+  const { email, password, firstName, lastName } = req.body;
 
   try {
     const encryptedPassword = await encryptString(password);
     const { _id } = await db.User.create({
       email: email,
       password: encryptedPassword,
-      firstName: firstname,
-      lastName: lastname,
+      firstName: firstName,
+      lastName: lastName,
       active: true,
     });
 
-    return res.status(200).send(
-      generateResponse({
-        data: {
-          id: _id,
-          email,
-        },
-      }),
-    );
+    return res.status(200).send({
+      id: _id,
+      email,
+    });
   } catch (err) {
     console.log(err);
     next(err);
@@ -31,15 +26,11 @@ async function signUp(req, res, next) {
 
 async function fetchUsers(req, res, next) {
   try {
-    const users = await db.User.find();
+    const users = await db.User.find().lean();
 
-    res.status(200).send(
-      generateResponse({
-        data: {
-          users: users,
-        },
-      }),
-    );
+    res.status(200).send({
+      data: users,
+    });
   } catch (error) {
     next(error);
   }
@@ -51,13 +42,64 @@ async function fetchUserById(req, res, next) {
   } = req;
 
   try {
-    const user = await db.User.findById(userId);
+    const user = await db.User.findById(userId).lean();
 
-    res.status(200).send(
-      generateResponse({
-        data: { user: user },
-      }),
-    );
+    res.status(200).send({
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function updateUser(req, res, next) {
+  const { id: userId } = req.params;
+  const { firstName, lastName } = req.body;
+
+  try {
+    const updatedUser = await db.User.findOneAndUpdate(
+      {
+        _id: userId,
+      },
+      {
+        $set: {
+          firstName: firstName,
+          lastName: lastName,
+        },
+      },
+      {
+        new: true,
+      },
+    ).select({
+      firstName: 1,
+      lastName: 1,
+    });
+
+    res.status(200).send({
+      data: updatedUser,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteUser(req, res, next) {
+  const { id: userId } = req.params;
+
+  try {
+    const result = await db.User.deleteOne({
+      _id: userId,
+    }).lean();
+
+    if (result.ok === 1 && result.deletedCount === 1) {
+      res.status(200).send({
+        data: "User removed",
+      });
+    } else {
+      res.status(500).send({
+        data: "User not removed",
+      });
+    }
   } catch (error) {
     next(error);
   }
@@ -67,4 +109,6 @@ module.exports = {
   signUp: signUp,
   fetchUsers: fetchUsers,
   fetchUserById: fetchUserById,
+  updateUser: updateUser,
+  deleteUser: deleteUser,
 };
