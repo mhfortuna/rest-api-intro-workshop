0dbccccafa6a540fb30ea43f2ae85817b6bf6f03
Jcarri14 <j.carrion@students.salle.url.edu>
2021-07-20 12:07:51 +0200

    feat: update user routes and controller

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,15 +3,15 @@ const { encryptString } = require("../utils/encrypt");
 const { generateResponse } = require("../utils/generateResponse");
 
 async function signUp(req, res, next) {
-  const { email, password, firstname, lastname } = req.body;
+  const { email, password, firstName, lastName } = req.body;
 
   try {
     const encryptedPassword = await encryptString(password);
     const { _id } = await db.User.create({
       email: email,
       password: encryptedPassword,
-      firstName: firstname,
-      lastName: lastname,
+      firstName: firstName,
+      lastName: lastName,
       active: true,
     });
 
@@ -24,7 +24,7 @@ async function signUp(req, res, next) {
       }),
     );
   } catch (err) {
-    console.log(err);
+    // console.log(err);
     next(err);
   }
 }
@@ -54,7 +54,6 @@ async function fetchUsers(req, res, next) {
 async function fetchUserById(req, res, next) {
   const {
     params: { id: userId },
-    query: { fullFetch },
   } = req;
 
   try {
@@ -78,8 +77,69 @@ async function fetchUserById(req, res, next) {
   }
 }
 
+async function updateUser(req, res, next) {
+  const { id: userId } = req.params;
+  const { firstName, lastName } = req.body;
+
+  try {
+    const updatedUser = await db.User.findOneAndUpdate(
+      {
+        _id: userId,
+      },
+      {
+        $set: {
+          firstName: firstName,
+          lastName: lastName,
+        },
+      },
+      {
+        new: true,
+      },
+    ).select({
+      firstName: 1,
+      lastName: 1,
+    });
+
+    res.status(200).send(
+      generateResponse({
+        data: updatedUser,
+      }),
+    );
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function deleteUser(req, res, next) {
+  const { id: userId } = req.params;
+
+  try {
+    const result = await db.User.deleteOne({
+      _id: userId,
+    }).lean();
+
+    if (result.ok === 1 && result.deletedCount === 1) {
+      res.status(200).send(
+        generateResponse({
+          data: "User removed",
+        }),
+      );
+    } else {
+      res.status(500).send(
+        generateResponse({
+          error: "User not removed",
+        }),
+      );
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
 module.exports = {
   signUp: signUp,
   fetchUsers: fetchUsers,
   fetchUserById: fetchUserById,
+  updateUser: updateUser,
+  deleteUser: deleteUser,
 };
