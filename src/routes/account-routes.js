19321a0935e3db1b89214476446c61906e0620e5
Joan Carrión Anaya <43220742+JCarri14@users.noreply.github.com>
2021-08-05 09:34:52 +0200

    feat: remove implementations

--- src/routes/account-routes.js
+++ src/routes/account-routes.js
@@ -1,12 +1,5 @@
 const Router = require("express").Router;
 
-const { authController, userController } = require("../controllers");
-
-const accountRouter = Router();
-
-accountRouter.post("/authenticate", authController.authenticate);
-accountRouter.post("/register", userController.signUp);
 
 module.exports = {
-  accountRouter: accountRouter,
 };
